9bcc655bfb5ab4c8b5be29851e109b8e496b0595
gc200362901 <200362901@student.georgianc.on.ca>
2018-04-10 22:28:44 -0400

    first commit

---
+++ StudentDirectoryAPI/Models/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentDirectoryAPI.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required]
+        public String LastName { get; set; }
+
+        [Required]
+        public String FirstName { get; set; }
+
+        [Required]
+        public String Address { get; set; }
+
+        [Required]
+        public String City { get; set; }
+
+        [Required]
+        public String Province { get; set; }
+
+        [Required]
+        public String Postal { get; set; }
+
+        [Required]
+        public String Phone { get; set; }
+
+    }
+}
---
+++ StudentDirectoryAPI/Models/StudentDirectoryModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentDirectoryAPI.Models
+{
+    public class StudentDirectoryModel : DbContext
+    {
+
+        public StudentDirectoryModel(DbContextOptions<StudentDirectoryModel>options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
